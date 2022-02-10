198c6e90a567445c67874044e260dece036a024d
sayqool <99399679+sayqool@users.noreply.github.com>
2022-02-10 20:23:50 +0900

    1.0

---
+++ js/start.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+        swal("test");
+  });
+  
+
+
+$('#' + 'btn-start-img').click(function(){
+  Swal.fire({
+    title: 'あなたたちは落合夫婦ですか？',
+    text: "このゲームは落合一輝兄弟と葉月姉妹のためのパーソナルなゲームです。",
+    type: 'warning',
+    showCancelButton: true,
+    confirmButtonText: '私たちは落合夫婦だ！',
+    cancelButtonText: '実はちがう...。'  
+  }).then((result) => {
+    if (result.value) {
+        window.location.href = "Question.html";
+    }
+  });
+  
+})
