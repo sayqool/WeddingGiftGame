87877b1e186a736bc0036aadd8362f4b2fe03f4c
sayqool <99399679+sayqool@users.noreply.github.com>
2022-04-14 00:49:22 +0900

    2画面目途中まで

--- js/2.js
+++ js/2.js
@@ -1,4 +1,36 @@
+$(document).ready(function(){
+  //処理
+  // const music = new Audio('/musics/music.mp3');
+  // music.play();
+  reize();
+});
+
+
+$(function() {
+
+  $(window).resize(function() {
+    //リサイズされたときの処理
+    reize();
+  });
+
+});
+
+
+function reize(){
+ 
+}
+
+
+let cntPage = 0;
+const nextPage = 2
 $('#' + 'btn-next').click(function(){
-    window.location.href = "3.html";
-  })
+    cntPage++;
+
+    if(cntPage < nextPage){
+      
+    }else{
+      window.location.href = "3.html";
+    }
+})
+  
   
