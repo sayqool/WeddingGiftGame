a88824c2f5e2bc12d7f2cde76e4e9c92c95e0b93
sayqool <99399679+sayqool@users.noreply.github.com>
2022-04-22 22:25:40 +0900

    修正

--- js/answer.js
+++ js/answer.js
@@ -20,8 +20,18 @@ $(document).ready(function(){
   
   function reize(){
     let intViewportHeight = window.innerHeight;
+    let intViewportWidth = window.innerWidth;
+    let boxWidth = intViewportWidth * 0.85 / 2;
     let boxHeight = intViewportHeight * 0.85;
     $('.box').height(intViewportHeight);
-    $('.chara-img').height(boxHeight*1.01);
+    $('.chara-img').height(boxHeight);
+    let statusW = $('.status-image').width();
+    let statusH = $('.status-image').height();
+    let bw = (boxWidth / 3);
+    let staPatio = bw / statusW;
+    $('.status-image').width(bw);
+    $('.status-image').height(statusH * staPatio);
+    $('.status-bar').width(bw * 2);
+    $('.status-bar').height(statusH * staPatio);
   }
   
