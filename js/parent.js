8542c897a00a5e4911d905575921cd8c9f38b703
saygood1087@gmail.com <saygood1087@gmail.com>
2022-05-03 23:49:47 +0900

    test

---
+++ js/parent.js
@@ -0,0 +1,30 @@
+// common const
+const ID_PARENT = "parent";
+const BODY = "body";
+
+
+$(document).ready(function(){
+    //処理
+    resize();
+
+    readFirstPage();
+  });
+
+  function resize(){
+    $(BODY).css("margin", 0);
+    let innerHeight = window.innerHeight;
+    let innerWidth = window.innerWidth;
+    
+    $("#" + ID_PARENT).height(innerHeight);
+    let hPatio = 768 / innerHeight;
+    let width = 1366;
+    if(innerWidth < 1366){
+      width = 1366 * hPatio;
+    }
+    $("#" + ID_PARENT).width(width);
+  }
+
+
+  function readFirstPage(){
+    $("#" + ID_PARENT).load("Index.html");
+  }
